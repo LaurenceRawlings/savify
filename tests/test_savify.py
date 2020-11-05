cef1b77445407680a00e013b900f47a1c0effd41
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-11-05 09:37:44 +0000

    Fix test

--- tests/test_savify.py
+++ tests/test_savify.py
@@ -15,7 +15,7 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'savify.cli.main' in result.output
+    assert 'Savify' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
