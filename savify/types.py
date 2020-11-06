80a6a42008d45275ee29c0d4e44570e39c7462bc
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-11-06 19:02:27 +0000

    Add additional format and quality options

--- savify/types.py
+++ savify/types.py
@@ -11,7 +11,20 @@ class Platform:
 
 class Format:
     MP3 = 'mp3'
+    AAC = 'aac'
+    FLAC = 'flac'
+    M4A = 'm4a'
+    OPUS = 'opus'
+    VORBIS = 'vorbis'
+    WAV = 'wav'
 
 
 class Quality:
+    WORST = '9'
+    Q32K = '32K'
+    Q96K = '96K'
+    Q128K = '128K'
+    Q192K = '192K'
+    Q256K = '256K'
+    Q320K =  '320K'
     BEST = '0'
