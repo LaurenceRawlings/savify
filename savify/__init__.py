85a3f0669b20ce22512728096d32b9d94587aeeb
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-11-11 12:36:34 +0000

    Bump version: 2.0.5 → 2.0.6

--- savify/__init__.py
+++ savify/__init__.py
@@ -13,7 +13,7 @@ from .types import *
 __title__ = 'Savify'
 __author__ = """Laurence Rawlings"""
 __email__ = 'contact@laurencerawlings.com'
-__version__ = '2.0.5'
+__version__ = '2.0.6'
 __license__ = 'MIT'
 __docformat__ = 'restructuredtext en'
 
