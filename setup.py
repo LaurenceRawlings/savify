3ff8e9c958cbf157cfb486997cc851eafe79561b
Laurence Rawlings <rawlingslaurence@gmail.com>
2021-01-29 00:52:21 +0000

    Bump version: 2.3.1 → 2.3.2

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/LaurenceRawlings/savify',
-    version='2.3.1',
+    version='2.3.2',
     zip_safe=False,
 )
