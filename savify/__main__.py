1dbe0dd9bd2cb8a80a187f6611d07ca1ee5371f4
Laurence Rawlings <rawlingslaurence@gmail.com>
2021-02-02 13:05:27 +0000

    Reformat

--- savify/__main__.py
+++ savify/__main__.py
@@ -2,6 +2,7 @@ import sys
 
 if __package__ is None and not hasattr(sys, 'frozen'):
     import os.path
+
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
